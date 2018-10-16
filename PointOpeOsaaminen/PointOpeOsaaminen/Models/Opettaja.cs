39283b4f14094edd46017ad1c04b0d7b40683fc1
Antti Nyman <antti.nyman@stud.pointcollege.fi>
2018-10-16 13:26:40 +0300

    Lisätty uusi azureen luotu tietokanta

--- PointOpeOsaaminen/PointOpeOsaaminen/Models/Opettaja.cs
+++ PointOpeOsaaminen/PointOpeOsaaminen/Models/Opettaja.cs
@@ -11,8 +11,7 @@ namespace PointOpeOsaaminen.Models
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-
+    
     public partial class Opettaja
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -28,15 +27,7 @@ namespace PointOpeOsaaminen.Models
         public string Henkilönumero { get; set; }
         public string Yksikkö { get; set; }
         public string Toimenkuva { get; set; }
-        
-        public string Nimi
-        {
-            get
-            {
-                return  Etunimi + " " + Sukunimi;
-            }
-        }
-
+    
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Osaamiset> Osaamiset { get; set; }
     }
