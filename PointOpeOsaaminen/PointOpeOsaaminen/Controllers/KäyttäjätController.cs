d86452036bacd93dd1626006570b61a5ca85e947
Sadam Jahein <sadam.jahein@stud.pointcollege.fi>
2018-11-06 21:52:02 +0200

    Login Ja logout luonti sivulle

---
+++ PointOpeOsaaminen/PointOpeOsaaminen/Controllers/KäyttäjätController.cs
@@ -0,0 +1,48 @@
+﻿using PointOpeOsaaminen.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PointOpeOsaaminen.Controllers
+{
+    public class KäyttäjätController : Controller
+    {
+        // GET: Käyttäjät
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorise(Käyttäjät u)
+        {
+            if (ModelState.IsValid)
+            {
+                using (LoginEntities db = new LoginEntities())
+                {
+
+                    var v = db.Käyttäjät.Where(a => a.KäyttäjäTunnus.Equals(u.KäyttäjäTunnus) && a.Salasana.Equals(u.Salasana)).FirstOrDefault();
+                    if (v != null)
+                    {
+                        Session["LogedKäyttäjäID"] = v.KäyttäjäID.ToString();
+                        Session["LogedUserSalasana"] = v.Salasana.ToString();
+                        return RedirectToAction("Index", "Home");
+                    }
+
+
+                }
+            }
+            {
+                return View();
+            }
+        }
+        public ActionResult Logout()
+        {
+            Session.Abandon();
+
+            return RedirectToAction("Index", "Käyttäjät");
+        }
+    }
+}
+    
