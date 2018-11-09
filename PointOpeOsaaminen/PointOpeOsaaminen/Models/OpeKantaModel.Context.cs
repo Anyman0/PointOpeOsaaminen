7068c227ff9cb342afbbb68c4d76455429bfe2d0
Yrjö Halinen <yrjo.halinen@stud.pointcollege.fi>
2018-11-09 16:02:24 +0200

    Yhteistiedot + Tietoja meistä päivitys

--- PointOpeOsaaminen/PointOpeOsaaminen/Models/OpeKantaModel.Context.cs
+++ PointOpeOsaaminen/PointOpeOsaaminen/Models/OpeKantaModel.Context.cs
@@ -27,6 +27,5 @@ namespace PointOpeOsaaminen.Models
     
         public virtual DbSet<Opettaja> Opettaja { get; set; }
         public virtual DbSet<Osaamiset> Osaamiset { get; set; }
-        public virtual DbSet<database_firewall_rules> database_firewall_rules { get; set; }
     }
 }
